e83b1a5ee18e83d4007af4aa5eb9163a177718ef
Samuel Millward <samuel.j.pearce@hotmail.com>
2022-04-17 19:29:07 +0100

    Add react-route-dom, error page routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+
 import AllPokemon from './AllPokemon'
 import Header from './components/Header'
+import ErrorPage from './components/ErrorPage'
 
 const App = () => {
   return (
     <>
       <div className="App">
         <Header />
-        <AllPokemon />
+        <Routes>
+          <Route path="/" element={<AllPokemon />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </div>
     </>
   )
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center">
+      Sorry. This page is not found.
+    </div>
+  )
+}
+
+export default ErrorPage
