1804eb6f4cd6ab341dbdd9baf8b011b6586797a5
Samuel Millward <samuel.j.pearce@hotmail.com>
2022-04-19 15:12:50 +0100

    PokemonCard renders favourites data

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,13 +1,27 @@
-import React, { useContext } from 'react'
-import PokemonContext from '../PokemonContext'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Favourites = () => {
-  const { favourites } = useContext(PokemonContext)
+  const pokemonData = JSON.parse(localStorage.getItem("Pokemon"));
 
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <div className="grid grid-cols-4 gap-x-64 gap-y-8">{/*  */}</div>
+        <div className="grid grid-cols-4 gap-x-64 gap-y-8">{pokemonData.map((pokemon) => {
+          return (
+            <div className="flex flex-col items-center">
+              <Link to={`/pokemoncard/${pokemon.id}`}>
+              <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              alt="Pokemon"
+            />
+              {pokemon.name.replace(/\b\w/g, (capitalFirstCharacter) =>
+                capitalFirstCharacter.toUpperCase()
+              )}
+              </Link>
+            </div>
+          )
+        })}</div>
       </div>
     </>
   )
